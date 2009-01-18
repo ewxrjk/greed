4fb5af194e8e3be229c333f06bdd7ac5faa02c98
Richard Kettlewell <rjk@greenend.org.uk>
2009-01-18 14:10:38 +0000

    Greed 1.4.3

--- greed.el
+++ greed.el
@@ -5,7 +5,7 @@
 ;;         Richard Kettlewell <rjk@greenend.org.uk>
 ;;         Peter Maydell <pmaydell@chiark.greenend.org.uk>
 ;; Created: 1 Oct 1995
-;; Version: 1.4.DEV
+;; Version: 1.4.3
 ;; Keywords: news
 
 ;; LCD Archive Entry:
@@ -53,7 +53,7 @@
 
 ;;; Code:
 
-(defconst greed-version "1.4.DEV")
+(defconst greed-version "1.4.3")
 
 
 ;;; Requirements: -------------------------------------------------------------
